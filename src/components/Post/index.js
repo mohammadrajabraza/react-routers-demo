7735b3b85114ea2a73927044d7ee1ca573c51633
Muhammad Rajab Raza <mohammadrajabraza@gmail.com>
2021-07-01 12:15:36 +0500

    record history for detail posts to redirect back after authentication

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -1,12 +1,13 @@
 import './index.css'
 import {Row, Col, Button, Card} from 'react-bootstrap'
-import {Link, useRouteMatch} from 'react-router-dom'
+import {Link, useRouteMatch, useHistory} from 'react-router-dom'
 
 
 function Post(props) {
 
     const { post : {title, id}, postIndex} = props
     let match = useRouteMatch()
+    let history = useHistory()
     return (
         <Card className="post"  style={{ width: 'auto' }}>
             <Card.Header className="header">{`Post ${postIndex + 1}`}</Card.Header>
@@ -14,7 +15,7 @@ function Post(props) {
                 <Card.Title>{title}</Card.Title>
                 <Row className="footer">
                 <Col className="col">
-                    <Link to={`${match.url}/${id}`}>
+                    <Link to={`${match.url}/${id}`} onClick={() => history.push(`${match.url}/${id}`)}>
                         <Button className="ml-2" variant="success">Detail</Button> 
                     </Link>
                 </Col>
