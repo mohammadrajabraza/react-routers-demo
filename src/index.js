f0cc9dafb07a798bbc64ceac7266b9f5e16449ac
Muhammad Rajab Raza <mohammadrajabraza@gmail.com>
2021-07-05 14:57:04 +0500

    removed web-vitals

--- src/index.js
+++ src/index.js
@@ -3,12 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import {BrowserRouter} from 'react-router-dom'
-import ScrollToTop from './utlities/ScrollToTop'
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter basename="router">
-      <ScrollToTop/>
+      
       <App />
     </BrowserRouter>
   </React.StrictMode>,
